94d154cce291e30fc3ce9149de6abddc4bd52729
Andrew <acyuan7@gmail.com>
2026-01-17 15:45:43 -0500

    workout types

---
+++ data/WorkoutTypes.ts
@@ -0,0 +1,24 @@
+
+type Workout = {
+    id: string;
+    name: string;
+    exercises: ExerciseSet[]
+    difficulty: "easy" | "medium" | "hard";
+    time: number // minutes
+}
+
+type ExerciseSet = {
+    id: string;
+    exercises: Exercise
+    num_sets: number
+    num_reps: number
+    rest_between: number // in seconds
+}
+
+type Exercise = {
+    id: string;
+    name: string;
+    threshold_data: object;
+    orientation_instructions: string; // how the user should be oriented relative to the camera
+}
+
